3e006de246782b0d907780ef082cf22fc6bc1a1c
Malik ATIF Ali <malikatifali94@gmail.com>
2025-08-01 11:59:52 +0500

    update data
    add some data

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
 
-      title: 'MineChat.ai',
+      title: 'MineChat.ai test',
       theme: ThemeData(
         // This is the theme of your application.
         //
