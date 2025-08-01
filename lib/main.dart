5c2aa16cad52a1a1687a90dc7ccb74518b78168d
Mubeen Bhatti <mub33n.bhatti@gmail.com>
2025-08-01 12:43:48 +0500

    Removed Spaces
    Removed Spaces in main.dart File

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
-
       title: 'MineChat.ai hello dear',
-
-
-    
-
-
       theme: ThemeData(
         // This is the theme of your application.
         //
