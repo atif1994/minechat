4a72df4acfd3d4338a4afcd17f4dbf847ff51650
Mubeen Bhatti <mub33n.bhatti@gmail.com>
2025-08-01 12:14:21 +0500

    ggg
    updata

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
 
-      title: 'MineChat.ai test',
+      title: 'MineChat.ai test purpose',
       theme: ThemeData(
         // This is the theme of your application.
         //
